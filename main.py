c36a7c107bab59be955f516283ce83818eddfecb
Karriem O. Lateef <kalateef@microsoft.com>
2023-06-28 12:01:04 -0400

    Update main.py
    Modifying the code to run against Azure OpenAI

--- main.py
+++ main.py
@@ -27,26 +27,19 @@ def main():
     if input_method == 'Enter a YouTube URL':
         youtube_url = st.text_input("Enter a YouTube URL to summarize")
 
-    api_key = st.text_input("Enter API key here, or contact the author if you don't have one.")
-    st.markdown('[Author email](mailto:ethanujohnston@gmail.com)')
-    use_gpt_4 = st.checkbox("Use GPT-4 for the final prompt (STRONGLY recommended, requires GPT-4 API access - progress bar will appear to get stuck as GPT-4 is slow)", value=True)
-    find_clusters = st.checkbox('Find optimal clusters (experimental, could save on token usage)', value=False)
-    st.sidebar.markdown('# Made by: [Ethan](https://github.com/e-johnstonn)')
-    st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/e-johnstonn/docsummarizer)')
-    st.sidebar.markdown("""<small>It's always good practice to verify that a website is safe before giving it your API key. 
-                        This site is open source, so you can check the code yourself, or run the streamlit app locally.</small>""", unsafe_allow_html=True)
-
+    st.sidebar.markdown('# Forked from: [Ethan](https://github.com/e-johnstonn)')
+    st.sidebar.markdown('# Git link: [Docsummarizer](https://github.com/kalateefMSFT/docsummarizer)')
 
     if st.button('Summarize (click once and wait)'):
         if input_method == 'Upload a document':
-            process_summarize_button(uploaded_file, api_key, use_gpt_4, find_clusters)
+            process_summarize_button(uploaded_file, find_clusters)
 
         else:
             doc = transcript_loader(youtube_url)
-            process_summarize_button(doc, api_key, use_gpt_4, find_clusters, file=False)
+            process_summarize_button(doc, find_clusters, file=False)
 
 
-def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_clusters, file=True):
+def process_summarize_button(file_or_transcript, find_clusters, file=True):
     """
     Processes the summarize button, and displays the summary if input and doc size are valid
 
@@ -60,7 +53,7 @@ def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_cluste
 
     :return: None
     """
-    if not validate_input(file_or_transcript, api_key, use_gpt_4):
+    if not validate_input(file_or_transcript):
         return
 
     with st.spinner("Summarizing... please wait..."):
@@ -73,7 +66,7 @@ def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_cluste
             doc = file_or_transcript
             map_prompt = youtube_map
             combine_prompt = youtube_combine
-        llm = create_chat_model(api_key, use_gpt_4)
+        llm = create_chat_model()
         initial_prompt_list = summary_prompt_creator(map_prompt, 'text', llm)
         final_prompt_list = summary_prompt_creator(combine_prompt, 'text', llm)
 
@@ -83,10 +76,10 @@ def process_summarize_button(file_or_transcript, api_key, use_gpt_4, find_cluste
             return
 
         if find_clusters:
-            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, api_key, use_gpt_4, find_clusters)
+            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, find_clusters)
 
         else:
-            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list, api_key, use_gpt_4)
+            summary = doc_to_final_summary(doc, 10, initial_prompt_list, final_prompt_list)
 
         st.markdown(summary, unsafe_allow_html=True)
         if file:
@@ -111,30 +104,17 @@ def validate_doc_size(doc):
     return True
 
 
-def validate_input(file_or_transcript, api_key, use_gpt_4):
+def validate_input(file_or_transcript):
     """
     Validates the user input, and displays warnings if the input is invalid
 
     :param file_or_transcript: The file uploaded by the user or the YouTube URL entered by the user
-
-    :param api_key: The API key entered by the user
-
-    :param use_gpt_4: Whether the user wants to use GPT-4
 
     :return: True if the input is valid, False otherwise
     """
     if file_or_transcript == None:
         st.warning("Please upload a file or enter a YouTube URL.")
         return False
-
-    if not check_key_validity(api_key):
-        st.warning('Key not valid or API is down.')
-        return False
-
-    if use_gpt_4 and not check_gpt_4(api_key):
-        st.warning('Key not valid for GPT-4.')
-        return False
-
     return True
 
 
